230020835d54ec3fa2e45ae6ef42807565ce363c
attetorres <atteneritorresgonzalez@gmail.com>
2026-01-17 12:34:07 +0000

    feat: crear fichero de rutas de users

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,21 @@
 import express from "express";
+import userRoutes from "./routes/usersRoutes.js";
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send({
+    api: "Yggdrasound",
+    version: "1.0.0",
+    endpoints: {
+      users: "/api/users",
+      vinyls: "/api/vinyls",
+    },
+  });
 });
+
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
---
+++ backend/src/routes/usersRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const router = express.Router();
+
+// GET
+router.get("/get-user/:id", getUserById);
+
+// POST
+
+// PUT
+
+export default router;

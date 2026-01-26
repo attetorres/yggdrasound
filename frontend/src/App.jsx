17d0ab94bebd6ab30a03574dcb329a59c30a7076
attetorres <atteneritorresgonzalez@gmail.com>
2026-01-26 14:08:01 +0000

    feat: crar paleta de colores con tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,5 @@
-import "./App.css";
-
 function App() {
-  return (
-    <>
-      <h1 class="text-3xl font-bold underline text-red-400">Hello world!</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;

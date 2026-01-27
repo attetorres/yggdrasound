836706f2bf3fa46c5c3731cc66239f4b8671ee58
attetorres <atteneritorresgonzalez@gmail.com>
2026-01-27 13:49:26 +0000

    feat: mejorar la vista principal del header

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,17 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+
+  const isLoginPage = location.pathname === "/login";
+  const isRegisterPage = location.pathname === "/register";
+
   return (
-    <header className="bg-neutral-950 text-white p-4">
+    <header className="flex bg-neutral-950 text-white p-4 justify-between">
       <nav>
-        <Link to="/" className="text-white mr-4 hover:text-gray-300">
+        <Link to="/" className="text-white mr-4 hover:text-primary-300">
           Home
         </Link>
-        <Link to="/login" className="text-white hover:text-gray-300">
-          Login
-        </Link>
       </nav>
+      <div className="space-x-4">
+        {isLoginPage ? (
+          <Link to="/register" className="text-white hover:text-primary-300">
+            Registro
+          </Link>
+        ) : isRegisterPage ? (
+          <Link to="/login" className="text-white hover:text-primary-300">
+            Login
+          </Link>
+        ) : (
+          <>
+            <Link to="/login" className="text-white hover:text-primary-300">
+              Login
+            </Link>
+            <span className="text-neutral-400">|</span>
+            <Link to="/register" className="text-white hover:text-primary-300">
+              Registro
+            </Link>
+          </>
+        )}
+      </div>
     </header>
   );
 };

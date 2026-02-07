a4c08127629ebbf88eb31bd9f23e65e4fd710305
attetorres <atteneritorresgonzalez@gmail.com>
2026-02-07 16:52:58 +0000

    feat: Crear ruta a login si no hemos iniciado sesión en la aplicación y queremos entrar en perfil desde url

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
 import MainLayout from "../components/layout/MainLayout";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
@@ -9,6 +10,8 @@ import Catalog from "../pages/Catalog";
 import VinylDetails from "../pages/VinylDetails";
 
 const AppRouter = () => {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,7 +19,10 @@ const AppRouter = () => {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/profile"
+            element={isLoggedIn ? <Profile /> : <Navigate to="/login" />}
+          />
           <Route path="/catalog" element={<Catalog />} />
           <Route path="/vinyl-details/:id" element={<VinylDetails />} />
         </Route>
